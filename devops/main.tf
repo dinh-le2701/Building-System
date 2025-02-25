79f60c33d6e905b9970d4fb94655537c356242e8
dinh-letat <dinhprococoa@gmail.com>
2025-02-25 09:02:09 +0700

    fix: update fe build

--- devops/main.tf
+++ devops/main.tf
@@ -1,19 +1,24 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.16"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
+  }
 }
 
 resource "aws_s3_bucket" "building" {
-    bucket = "terraform-s3-bucket-building"
+  bucket = "terraform-s3-bucket-building"
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [tags]
+  }
 }
 
 resource "aws_s3_bucket_versioning" "version" {
-    bucket = aws_s3_bucket.building.id
-    versioning_configuration {
-        status = "Enabled"
-    }
+  bucket = aws_s3_bucket.building.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
