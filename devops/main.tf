690a497919da38d19af9d23edf241fb12f9e099b
dinh-letat <dinhprococoa@gmail.com>
2025-02-25 09:47:17 +0700

    fix: update pipeline

--- devops/main.tf
+++ devops/main.tf
@@ -7,23 +7,35 @@ terraform {
   }
 }
 
+data "aws_s3_bucket" "existing" {
+  bucket = "terraform-s3-bucket-building"
+}
+
 resource "aws_s3_bucket" "building" {
   bucket = "terraform-s3-bucket-building"
 
   lifecycle {
     prevent_destroy = true
   }
+
+  # Chỉ tạo nếu bucket chưa tồn tại
+  count = length(data.aws_s3_bucket.existing.id) == 0 ? 1 : 0
 }
 
+locals {
+  bucket_id = length(aws_s3_bucket.building) > 0 ? aws_s3_bucket.building[0].id : data.aws_s3_bucket.existing.id
+}
+
+
 resource "aws_s3_bucket_versioning" "version" {
-  bucket = aws_s3_bucket.building.id
+  bucket = local.bucket_id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.building.id
+  bucket = local.bucket_id
 
   block_public_acls       = true
   block_public_policy     = true
